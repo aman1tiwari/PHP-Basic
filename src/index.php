ab5e35f0e8e12ad8f4ce6295ece3b2dd9d2f6e78
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-15 12:13:14 +0530

    Linked the pages

--- src/index.php
+++ src/index.php
@@ -19,7 +19,11 @@
     foreach ($original as $x) 
     {echo "$x ";}
     echo "\n"
-
     ?>
+    <br><br><br>
+    <a href="./temperature.php">Click to see Average Temperature Task</a><br>
+    <a href="./filterout.php">Click to see filterout task</a><br>
+    <a href="./delete.php">Click to see delete task</a><br>
+
 </body>
 </html>
