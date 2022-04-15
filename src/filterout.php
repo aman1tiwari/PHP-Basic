ab5c36509fcd693118483454e1310e4ff29a9668
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-15 11:57:39 +0530

    Filter out

---
+++ src/filterout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filter Out Elements</title>
+</head>
+<body>
+    <?php
+    $first_array=array('c1' => 'Red', 'c2' => 'Green', 'c3' => 'White', 'c4' => 'Black');
+    $second_array=array('c2','c4');
+    $filtered_array=array_diff_key($first_array, array_flip( $second_array));
+
+    print_r ($filtered_array);
+
+    ?>
+</body>
+</html>
